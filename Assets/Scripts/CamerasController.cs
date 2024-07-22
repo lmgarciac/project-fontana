50faa82cfc24e3400e0f5bfa0b91370116d06b7b
LuisGarcia <30421890+lmgarciac@users.noreply.github.com>
2024-07-22 08:04:30 -0300

    Cameras Controller
    Cameras Controller

---
+++ Assets/Scripts/CamerasController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraType
+{
+    Skybox = 0,
+    Player,
+}
+
+[Serializable]
+public struct CameraTypes
+{
+    public CameraType CameraType;
+    public Camera camera;
+}
+
+public class CamerasController : MonoBehaviour
+{
+    public CameraTypes[] cameras; 
+    private CameraType currentCamera = 0;
+    private CameraType previousCamera = 0;
+
+    void Start()
+    {
+        SetCamera(CameraType.Player);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (currentCamera == CameraType.Player)
+                SetCamera(CameraType.Skybox);
+            else
+                SetCamera(CameraType.Player);
+        }
+    }
+
+    public void SetCamera(CameraType cameraSelected)
+    {
+        foreach (CameraTypes cam in cameras)
+        {
+            cam.camera.enabled = false;
+            if (cam.CameraType == cameraSelected)
+            {
+                cam.camera.enabled = true;
+                previousCamera = currentCamera;
+                currentCamera = cameraSelected;
+
+                if (cameraSelected == CameraType.Player)
+                {
+                    GlobalManager.Instance.EnableCharacterController(true);
+                    GlobalManager.Instance.EnableFirstPersonController(true);
+                }
+                else
+                {
+                    GlobalManager.Instance.EnableCharacterController(false);
+                    GlobalManager.Instance.EnableFirstPersonController(false);
+                }
+            }
+        }
+    }
+}
