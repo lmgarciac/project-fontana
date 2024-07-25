2298591455834d569e765c62854910ffcf31ac26
LuisGarcia <30421890+lmgarciac@users.noreply.github.com>
2024-07-25 08:04:12 -0300

    Custom Cursor (Almost Done)
    Custom Cursor (Almost Done)

--- Assets/Scripts/CustomCursor.cs
+++ Assets/Scripts/CustomCursor.cs
@@ -3,21 +3,32 @@ using UnityEngine;
 public class CustomCursor : MonoBehaviour
 {
     [SerializeField] private Texture2D customCursorTexture;
-    [SerializeField] private ParticleSystem cursorTrail;
+    [SerializeField] private Camera targetCamera;
 
-    private CursorMode cursorMode = CursorMode.Auto;
+    [Range(5f, 100f)]
+    [SerializeField] private float distanceFromCamera;
+
     private Vector2 hotSpot = Vector2.zero;
+    private CursorMode cursorMode = CursorMode.Auto;
 
     private void Start()
     {
+        distanceFromCamera = 20f;
+        hotSpot = new Vector2(customCursorTexture.width / 2, customCursorTexture.height / 2);
         Cursor.SetCursor(customCursorTexture, hotSpot, cursorMode);
     }
 
     private void Update()
     {
+        // Convert mouse position to world position
         Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        mousePosition.z = 0;
-        this.transform.position = mousePosition;
+        //mousePosition.z = targetCamera.nearClipPlane + distanceFromCamera; // Set the distance from the camera
+        mousePosition.z = distanceFromCamera; // Set the distance from the camera
+
+        Vector3 worldPosition = targetCamera.ScreenToWorldPoint(mousePosition);
+
+        // Move the trail object to the mouse position
+        transform.position = worldPosition;
     }
 }
+
