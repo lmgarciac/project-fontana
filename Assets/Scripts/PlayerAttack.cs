2c1a1cbc2642f73b306b56d707b66bde894c2194
Benjamin Leonardi <54080730+BenjaLeonardi@users.noreply.github.com>
2024-07-25 01:45:34 -0300

    Added starting brush attack logic
    Will be working on animations soon probably.
    For now, only one random painting is hittable for testing purposes.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float attackDistance = 3f;
+    public float attackDelay = 0.1f;
+    public float attackSpeed = 0.3f;
+    public int attackDamage = 1; // For later combination with colour mixing
+    public LayerMask attackLayer;
+
+    public AudioClip brushSwing;
+    public AudioClip hitSound;
+
+    [SerializeField]
+    private Transform playerLineOfSight; //This can go into PlayerInteractions too
+
+    bool attacking = false;
+    bool readyToAttack = true;
+    int attackCount;
+
+    PlayerControls playerControls;
+    AudioSource audioSource;
+
+    public void Attack()
+    {
+        if (!readyToAttack || attacking) return;
+
+        readyToAttack = false;
+        attacking = true;
+
+        Invoke(nameof(ResetAttack), attackDelay);
+        Invoke(nameof(AttackRaycast), attackSpeed);
+
+        audioSource.pitch = Random.Range(0.95f, 1.15f);
+        audioSource.PlayOneShot(brushSwing);
+    }
+
+    void ResetAttack()
+    {
+        attacking = false;
+        readyToAttack = true;
+    }
+
+    private void HitTarget(Vector3 point)
+    {
+        audioSource.pitch = 1;
+        audioSource.PlayOneShot(hitSound);
+
+        Debug.Log("HIT ON TARGET");
+    }
+
+    void AttackRaycast()
+    {
+        if(Physics.Raycast(playerLineOfSight.position, playerLineOfSight.forward, out RaycastHit hit, attackDistance, attackLayer))
+        {
+            HitTarget(hit.point);
+        }
+    }
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (playerControls.Player.Attack.WasPressedThisFrame())
+        {
+            Attack();
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
