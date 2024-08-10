0486a2910291f9c5daad7c03ab122fca91cb849f
Benjamin Leonardi <benjamin@3ogs.com.ar>
2024-08-10 00:20:37 -0300

    Tidied up some code and made some prefab variants for the projectiles
    Tidied up some code and made some prefab variants for the projectiles

--- Assets/Scripts/PlayerProjectileAttack.cs
+++ Assets/Scripts/PlayerProjectileAttack.cs
@@ -1,13 +1,15 @@
 using Utils;
 using UnityEngine;
+using System;
 
 public class PlayerProjectileAttack : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject[] projectilePrefabs;
     [SerializeField] private Transform projectileSpawnPoint;
 
     private Vector3 projectileDestination;
+    private GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
@@ -37,11 +39,44 @@ public class PlayerProjectileAttack : MonoBehaviour
 
     private void InstantiateProjectile(Transform spawnPoint)
     {
+        CheckCurrentColor();
         GameObject projectileObj = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
 
         ProjectileLogic projectileSpawned = projectileObj.GetComponent<ProjectileLogic>();
         projectileSpawned.SetProjectileDestination((projectileDestination - spawnPoint.position).normalized);
+    }
 
-        projectileSpawned.ProjectileColor = GlobalManager.Instance.CurrentColorType; //Current mixed color
+    private void CheckCurrentColor()
+    {
+        switch (GlobalManager.Instance.CurrentColorType) //There must be a better way to do this but I'm falling asleep rn
+        {
+            case ColorType.Red:
+                projectilePrefab = projectilePrefabs[0];
+                break;
+
+            case ColorType.Yellow:
+                projectilePrefab = projectilePrefabs[1];
+                break;
+
+            case ColorType.Blue:
+                projectilePrefab = projectilePrefabs[2];
+                break;
+
+            case ColorType.Orange:
+                projectilePrefab = projectilePrefabs[3];
+                break;
+
+            case ColorType.Purple:
+                projectilePrefab = projectilePrefabs[4];
+                break;
+
+            case ColorType.Green:
+                projectilePrefab = projectilePrefabs[5];
+                break;
+
+            case ColorType.White:
+                projectilePrefab = projectilePrefabs[6];
+                break;
+        }
     }
 }
--- Assets/Scripts/ProjectileLogic.cs
+++ Assets/Scripts/ProjectileLogic.cs
@@ -8,21 +8,14 @@ public class ProjectileLogic : MonoBehaviour
     [SerializeField] private float projectileSpeed;
     [SerializeField] private float projectileDamage;
     [SerializeField] private float projectileLifetime;
+    [SerializeField] private ColorType projectileColor;
 
-    private ColorType projectileColor;
-    private bool collided;
     private Vector3 destinationPoint;
-    private ParticleSystem beamParticles;
-
-    public ColorType ProjectileColor { get => projectileColor; set => projectileColor = value; }
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody>().velocity = destinationPoint * projectileSpeed;
-        beamParticles = GetComponent<ParticleSystem>();
-        ChangeBeamColor(GlobalManager.Instance.CurrentColorType); //Grabs color at the time of spawning, could result in some bugs. Check it later
-
         Destroy(this.gameObject, projectileLifetime);
     }
 
@@ -36,56 +29,9 @@ public class ProjectileLogic : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.TryGetComponent<EnemyTest>(out EnemyTest enemyToHit);
-            enemyToHit.DamageHealth(projectileDamage, ProjectileColor);
+            enemyToHit.DamageHealth(projectileDamage, projectileColor);
 
             Destroy(gameObject);
         }
-
-        //if (collision.gameObject.CompareTag("Projectile") && collision.gameObject.CompareTag("Player") && !collided)
-        //{
-        //    if(collision.gameObject.TryGetComponent<EnemyTest>(out EnemyTest enemyToHit))
-        //    {
-        //        enemyToHit.DamageHealth(projectileDamage, ProjectileColor);
-        //    }
-
-        //    collided = true; //To limit to first collision
-        //    Destroy(gameObject);
-        //}
     }
-
-    void ChangeBeamColor(ColorType beamColor) //Bugged code, rework needed
-    {
-        //switch (beamColor) //There must be a better way to do this apart from this and a damage values matrix
-        //{
-        //    case ColorType.Red:
-        //        beamParticles.startColor = Color.red; //I know this is obsolete, but its the only way I can make it work
-        //        break;
-
-        //    case ColorType.Yellow:
-        //        beamParticles.startColor = Color.yellow;
-        //        break;
-
-        //    case ColorType.Blue:
-        //        beamParticles.startColor = Color.blue;
-        //        break;
-
-        //    case ColorType.Orange:
-        //        beamParticles.startColor = new Color(1, 0.2f, 0, 1); //Orange
-        //        break;
-
-        //    case ColorType.Purple:
-        //        beamParticles.startColor = new Color(1, 0, 1, 1);
-        //        break;
-
-        //    case ColorType.Green:
-        //        beamParticles.startColor = Color.green;
-        //        break;
-
-        //    case ColorType.White:
-        //        beamParticles.startColor = Color.white;
-        //        break;
-        //}
-    }
-
-
 }
